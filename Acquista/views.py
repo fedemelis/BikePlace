64d639b0dd5c35a55b76410ecafd296168028593
Federico Melis <fedemelis1@gmail.com>
2023-06-27 22:06:18 +0200

    test + bug fix

--- Acquista/views.py
+++ Acquista/views.py
@@ -98,7 +98,7 @@ class HomeAcquistiView(GroupRequiredMixin, TemplateView):
                 if self.request.user.email:
                     asyncio.run(sendMail(
                         "Nuovo sconto disponibile",
-                        "Ciao, è disponibile uno sconto per una bici che potrebbe interessarti. Vai su BikePlace per maggiori informazioni.",
+                        f"Ciao, è disponibile uno sconto per una {existing_discount.bike.type_of_bike} che potrebbe interessarti. Vai su BikePlace per maggiori informazioni.",
                         "fedemelis1@gmail.com",
                         [self.request.user.email]
                     ))
@@ -111,7 +111,7 @@ class HomeAcquistiView(GroupRequiredMixin, TemplateView):
                 if self.request.user.email:
                     asyncio.run(sendMail(
                         "Nuovo sconto disponibile",
-                        "Ciao, è disponibile uno sconto per una bici che potrebbe interessarti. Vai su BikePlace per maggiori informazioni.",
+                        f"Ciao, è disponibile uno sconto per una {bike_discount.bike.type_of_bike} che potrebbe interessarti. Vai su BikePlace per maggiori informazioni.",
                         "fedemelis1@gmail.com",
                         [self.request.user.email]
                     ))
@@ -324,7 +324,6 @@ class OrderConfirmationView(GroupRequiredMixin, View):
             total += item.bike.price
 
             if not item.bike.brand == "Creata da me":
-                print("HO ELIMINATOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
                 Bike.objects.filter(pk=item.bike.pk).delete()
 
             # Aggiungi l'istanza di SoldBike all'ordine
@@ -348,6 +347,7 @@ class OrderListView(GroupRequiredMixin, ListView):
     model = Order
     template_name = "order_list.html"
     context_object_name = "order_list"
+    paginate_by = 3
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user).order_by('-order_date')
